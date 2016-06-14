b7c7a42ceb62584e5627b0be61650e83dad1aa32
Matthew Solarski <mxtthxw@gmail.com>
2016-06-14 17:30:35 -0400

    Added Bootstrap buttons and corresponding JS so user can change pony color scheme.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
+// TO DO:
+// fix duration
+// make first song info display on page load
+
+
 $(document).ready(function(){
 
 checkContainer();
@@ -9,6 +14,97 @@ function checkContainer() {
     setTimeout(checkContainer, 50);
   }
 }
+
+$(".twilight-sparkle").on("click", function(){
+	// $(".headline").css("color", "#653188");
+	$(".t1").css("color", "#653188");
+	$(".t2").css("color", "#653188");
+	$(".t3").css("color", "#653188");
+	$(".t4").css("color", "#653188");
+	$(".t5").css("color", "#653188");
+	$(".t6").css("color", "#653188");
+	$(".pony-buttons").css("color", "#EE4D90");
+	$("body").css("background", "#D0A1E1");
+	$(".player").css("border", "10px solid #653188");
+	$(".info").css("color", "#EE4D90");
+	$("#pony-image").attr("src", "images/twilight_sparkle2.png");
+})
+
+$(".applejack").on("click", function(){
+	// $(".headline").css("color", "#F5EFB3");
+	$(".t1").css("color", "#F5EFB3");
+	$(".t2").css("color", "#F5EFB3");
+	$(".t3").css("color", "#F5EFB3");
+	$(".t4").css("color", "#F5EFB3");
+	$(".t5").css("color", "#F5EFB3");
+	$(".t6").css("color", "#F5EFB3");
+	$(".pony-buttons").css("color", "#EA403F");
+	$("body").css("background", "#F8B968");
+	$(".player").css("border", "10px solid #F5EFB3");
+	$(".info").css("color", "#EA403F");
+	$("#pony-image").attr("src", "images/applejack1.png");
+})
+
+$(".rainbow-dash").on("click", function(){
+	// $(".headline").css("color", "#FFF");
+	$(".t1").css("color", "#F13E32");
+	$(".t2").css("color", "#F67634");
+	$(".t3").css("color", "#F5EB7E");
+	$(".t4").css("color", "#7AC240");
+	$(".t5").css("color", "#1A96D4");
+	$(".t6").css("color", "#662E8A");
+	$(".pony-buttons").css("color", "#0193CF");
+	$("body").css("background", "#9DD9F8");
+	$(".player").css("border", "10px solid #0193CF");
+	// $(".info").css("color", "#FFF");
+	$("#pony-image").attr("src", "images/rainbow_dash1.png");
+})
+
+$(".rarity").on("click", function(){
+	// $(".headline").css("color", "#F5EFB3");
+	$(".t1").css("color", "#654F9D");
+	$(".t2").css("color", "#654F9D");
+	$(".t3").css("color", "#654F9D");
+	$(".t4").css("color", "#654F9D");
+	$(".t5").css("color", "#654F9D");
+	$(".t6").css("color", "#654F9D");
+	$(".pony-buttons").css("color", "#75D7FD");
+	// $("body").css("background", "#E8EDF0");
+	$("body").css("background", "#FFF");
+	$(".player").css("border", "10px solid #654F9D");
+	$(".info").css("color", "#75D7FD");
+	$("#pony-image").attr("src", "images/rarity1.png");
+})
+
+$(".fluttershy").on("click", function(){
+	// $(".headline").css("color", "#F5EFB3");
+	$(".t1").css("color", "#F9BED3");
+	$(".t2").css("color", "#F9BED3");
+	$(".t3").css("color", "#F9BED3");
+	$(".t4").css("color", "#F9BED3");
+	$(".t5").css("color", "#F9BED3");
+	$(".t6").css("color", "#F9BED3");
+	$(".pony-buttons").css("color", "#EC90AE");
+	$("body").css("background", "#FDF2AE");
+	$(".player").css("border", "10px solid #F9BED3");
+	$(".info").css("color", "#EC90AE");
+	$("#pony-image").attr("src", "images/fluttershy1.png");
+})
+
+$(".pinkie-pie").on("click", function(){
+	// $(".headline").css("color", "#F5EFB3");
+	$(".t1").css("color", "#EF519A");
+	$(".t2").css("color", "#EF519A");
+	$(".t3").css("color", "#EF519A");
+	$(".t4").css("color", "#EF519A");
+	$(".t5").css("color", "#EF519A");
+	$(".t6").css("color", "#EF519A");
+	$(".pony-buttons").css("color", "#C52270");
+	$("body").css("background", "#F7C2DA");
+	$(".player").css("border", "10px solid #EF519A");
+	$(".info").css("color", "#C52270");
+	$("#pony-image").attr("src", "images/pinkie_pie1.png");
+})
 
 // sample play function (for testing)
 // $(".play").on("click", function(){
